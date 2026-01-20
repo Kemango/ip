b1bd71e8068d7635a7a2b3324581f6311ca0d190
Guo Keming <e1398747@u.nus.edu>
2026-01-21 01:58:44 +0800

    Level 0: Rename, Greet, Exit

---
+++ src/main/java/Natto.java
@@ -0,0 +1,9 @@
+public class Natto {
+    public static void main(String[] args) {
+        String logo = "Natto";
+        String exit = "Bye. Hope to see you again soon!";
+        System.out.println("Hello I'm " + logo + "\n"
+        + "What can I do for you?");
+        System.out.println("\n" + exit);
+    }
+}
