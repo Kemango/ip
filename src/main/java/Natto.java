eac792c86062462476e1c44652c4938c70968a80
Guo Keming <e1398747@u.nus.edu>
2026-01-21 21:55:51 +0800

    Level-5: Handle Errors

--- src/main/java/Natto.java
+++ src/main/java/Natto.java
@@ -1,5 +1,12 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Set;
+
+class NattoException extends Exception {
+    public NattoException(String message) {
+        super(message);
+    }
+}
 
 class Task {
     String name;
@@ -71,6 +78,7 @@ class Event extends Task {
         return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
     }
 }
+
 public class Natto {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -82,66 +90,99 @@ public class Natto {
         // Start - Greeting User
         System.out.println("Hello! I'm " + logo);
         System.out.println("What can I do for you?");
-
+        Set<String> keywords = Set.of(
+                "bye", "mark", "unmark", "todo", "deadline", "event", "list"
+        );
         // Body
         while (true) {
-            String line = sc.nextLine();
-            String[] parts = line.split(" ");
-
-            if (line.equals("bye")) break;                            // Exit
-            if (parts[0].equals("mark")){                             // Mark
-                System.out.println("\nNice! I've marked this task as done:");
-                int index = Integer.parseInt(parts[1]) - 1;
-                tasks.get(index).mark();
-                System.out.println(tasks.get(index).toString());
-                continue;
-            }
-            if (parts[0].equals("unmark")){                           // Unmark
-                System.out.println("\nOK, I've marked this task as not done yet:");
-                int index = Integer.parseInt(parts[1]) - 1;
-                tasks.get(index).unmark();
-                System.out.println(tasks.get(index).toString());
-                continue;
-            }
-            if (parts[0].equals("todo")) {                            // Add Todo
-                String s = line.substring(5);
-                Todo theTask = new Todo(s);
-                System.out.println("\nGot it. I've added this task:");
-                tasks.add(theTask);
-                amountOfTasks = tasks.size();
-                System.out.println("   " + theTask);
-                System.out.println("Now you have " + amountOfTasks + " tasks in the list.");
-                continue;
-            }
-            if(parts[0].equals("deadline")) {                        // Add Deadline
-                String s = line.substring(9, line.indexOf("/by")).trim();
-                String date = line.substring(line.indexOf("/by") + 3).trim();
-                Deadline theTask = new Deadline(s ,date);
-                System.out.println("\nGot it. I've added this task:");
-                tasks.add(theTask);
-                amountOfTasks = tasks.size();
-                System.out.println("   " + theTask);
-                System.out.println("Now you have " + amountOfTasks + " tasks in the list.");
-            }
-            if(parts[0].equals("event")) {
-                String s = line.substring(6, line.indexOf("/from")).trim();
-                String from = line.substring(line.indexOf("/from") + 5, line.indexOf("/to")).trim();
-                String to = line.substring(line.indexOf("/to") + 3).trim();
-                Event theTask = new Event(s ,from, to);
-                System.out.println("\nGot it. I've added this task:");
-                tasks.add(theTask);
-                amountOfTasks = tasks.size();
-                System.out.println("   " + theTask);
-                System.out.println("Now you have " + amountOfTasks + " tasks in the list.");
-            }
-
-            if (line.equals("list")){                                 // Print history
-                System.out.println("____________________________________________________________");
-                System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println((i+1) + ". " + tasks.get(i));
+            try {
+                String line = sc.nextLine();
+                String[] parts = line.split(" ");
+                if(!keywords.contains(parts[0])){
+                    throw new NattoException("Please use a keyword like: todo, deadeline, event, list");
                 }
-                System.out.println("____________________________________________________________");
+
+                if (line.equals("bye")) break;                            // Exit
+                if (parts[0].equals("mark")){                             // Mark
+                    if (parts.length < 2) {
+                        throw new NattoException("Please specify a task index.");
+                    }
+                    int index = Integer.parseInt(parts[1]) - 1;
+                    if (index < 0 || index >= tasks.size()) {
+                        throw new NattoException("No such task exists.");
+                    }
+                    System.out.println("\nNice! I've marked this task as done:");
+                    tasks.get(index).mark();
+                    System.out.println(tasks.get(index).toString());
+                    continue;
+                }
+                if (parts[0].equals("unmark")){                           // Unmark
+                    if (parts.length < 2) {
+                        throw new NattoException("Please specify a task index.");
+                    }
+                    int index = Integer.parseInt(parts[1]) - 1;
+                    if (index < 0 || index >= tasks.size()) {
+                        throw new NattoException("No such task exists.");
+                    }
+                    System.out.println("\nOK, I've marked this task as not done yet:");
+                    tasks.get(index).unmark();
+                    System.out.println(tasks.get(index).toString());
+                    continue;
+                }
+                if (parts[0].equals("todo")) {                            // Add Todo
+                    if (parts.length < 2) {
+                        throw new NattoException("The description of a todo cannot be empty.");
+                    }
+                    String s = line.substring(5);
+                    Todo theTask = new Todo(s);
+                    System.out.println("\nGot it. I've added this task:");
+                    tasks.add(theTask);
+                    amountOfTasks = tasks.size();
+                    System.out.println("   " + theTask);
+                    System.out.println("Now you have " + amountOfTasks + " tasks in the list.");
+                    continue;
+                }
+                if(parts[0].equals("deadline")) {                        // Add Deadline
+                    if (parts.length < 2) {
+                        throw new NattoException("The description of a deadline cannot be empty.");
+                    }
+                    String s = line.substring(9, line.indexOf("/by")).trim();
+                    String date = line.substring(line.indexOf("/by") + 3).trim();
+                    Deadline theTask = new Deadline(s ,date);
+                    System.out.println("\nGot it. I've added this task:");
+                    tasks.add(theTask);
+                    amountOfTasks = tasks.size();
+                    System.out.println("   " + theTask);
+                    System.out.println("Now you have " + amountOfTasks + " tasks in the list.");
+                }
+                if(parts[0].equals("event")) {
+                    if (parts.length < 2) {
+                        throw new NattoException("The description of a event cannot be empty.");
+                    }
+                    String s = line.substring(6, line.indexOf("/from")).trim();
+                    String from = line.substring(line.indexOf("/from") + 5, line.indexOf("/to")).trim();
+                    String to = line.substring(line.indexOf("/to") + 3).trim();
+                    Event theTask = new Event(s ,from, to);
+                    System.out.println("\nGot it. I've added this task:");
+                    tasks.add(theTask);
+                    amountOfTasks = tasks.size();
+                    System.out.println("   " + theTask);
+                    System.out.println("Now you have " + amountOfTasks + " tasks in the list.");
+                }
+
+                if (parts[0].equals("list")) {                                 // Print history
+                    if (parts.length > 1) {
+                        throw new NattoException("list keyword works alone");
+                    }
+                    System.out.println("____________________________________________________________");
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println((i + 1) + ". " + tasks.get(i));
+                    }
+                    System.out.println("____________________________________________________________");
+                }
+            } catch (NattoException e) {
+                System.out.println(e.getMessage());
             }
         }
 
