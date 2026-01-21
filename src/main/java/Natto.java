63471bbcba8b55e1624faed3d88623f4d29da9b0
Guo Keming <e1398747@u.nus.edu>
2026-01-21 15:58:34 +0800

    Level-1: Echo

--- src/main/java/Natto.java
+++ src/main/java/Natto.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 public class Natto {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String logo = "Natto";
         String exit = "Bye. Hope to see you again soon!";
+        // Start - Greeting User
         System.out.println("Hello I'm " + logo + "\n"
-        + "What can I do for you?");
-        System.out.println("\n" + exit);
+                + "What can I do for you?");
+        // Body
+        while (true) {
+            String line = sc.nextLine();
+            if (line.equals("bye")) break;     // Exit
+            System.out.println(line);
+        }
+        //Ending
+        System.out.println(exit);
     }
 }
