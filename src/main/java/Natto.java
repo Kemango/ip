2daacc134151d0ec88b0021a1b1ea958a203fa6c
Guo Keming <e1398747@u.nus.edu>
2026-01-21 16:20:31 +0800

    Level-2: Add,List

--- src/main/java/Natto.java
+++ src/main/java/Natto.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Natto {
@@ -5,14 +6,23 @@ public class Natto {
         Scanner sc = new Scanner(System.in);
         String logo = "Natto";
         String exit = "Bye. Hope to see you again soon!";
+        ArrayList<String> history = new ArrayList<>();
+
         // Start - Greeting User
-        System.out.println("Hello I'm " + logo + "\n"
+        System.out.println("Hello! I'm " + logo + "\n"
                 + "What can I do for you?");
         // Body
         while (true) {
             String line = sc.nextLine();
             if (line.equals("bye")) break;     // Exit
-            System.out.println(line);
+            if (line.equals("list")){          // Print history
+                for (int i = 0; i < history.size(); i++) {
+                    System.out.println((i+1) + ". " + history.get(i));
+                }
+                continue;
+            }
+            System.out.println("added: " + line);
+            history.add(line);
         }
         //Ending
         System.out.println(exit);
