f054fedfa9e619e737327f7616c2ebbefe188156
Guo Keming <e1398747@u.nus.edu>
2026-01-21 16:41:32 +0800

    Level-3: Mark as Done (edit list)

--- src/main/java/Natto.java
+++ src/main/java/Natto.java
@@ -1,29 +1,54 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+class Task {
+    String name;
+    boolean isDone;
+
+    public Task(String name) {
+        this.name = name;
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        if(isDone) {
+            return ("[X] " + this.name);
+        } else {
+            return ("[ ] " + this.name);
+        }
+    }
+
+}
+
 public class Natto {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String logo = "Natto";
         String exit = "Bye. Hope to see you again soon!";
-        ArrayList<String> history = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
 
         // Start - Greeting User
         System.out.println("Hello! I'm " + logo);
         System.out.println("What can I do for you?");
+
         // Body
         while (true) {
             String line = sc.nextLine();
             if (line.equals("bye")) break;     // Exit
             if (line.equals("list")){          // Print history
-                for (int i = 0; i < history.size(); i++) {
-                    System.out.println((i+1) + ". " + history.get(i));
+                System.out.println("____________________________________________________________");
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println((i+1) + ". " + tasks.get(i));
                 }
+                System.out.println("____________________________________________________________");
                 continue;
             }
             System.out.println("added: " + line);
-            history.add(line);
+            tasks.add(new Task(line));
         }
+
         //Ending
         System.out.println(exit);
     }
